2c203fad845dc802b5c1764c27d7beb921136243
Mark Gibaud <mark.gibaud@novata.com>
2024-01-13 15:42:16 +0000

    weight watcher xls lives!

---
+++ sst.config.ts
@@ -0,0 +1,14 @@
+import { SSTConfig } from "sst";
+import { API } from "./stacks/MyStack";
+
+export default {
+  config(_input) {
+    return {
+      name: "weight-watcher-xls",
+      region: "us-east-1",
+    };
+  },
+  stacks(app) {
+    app.stack(API);
+  }
+} satisfies SSTConfig;
